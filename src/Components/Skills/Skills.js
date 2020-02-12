b3630a824375fe778eb73c3b9fbab5d2d1750155
Igor Izviekov <igorizviekov@gmail.com>
2020-02-12 19:34:07 +0200

    finished mobile version of home page

---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from "react";
+import { ReactComponent as Pen } from "../../Assets/Pen.svg";
+import { ReactComponent as Code } from "../../Assets/Code.svg";
+export default function Skills() {
+  return (
+    <Fragment>
+      <br />
+      <br />
+      <h1>What do I use?</h1>
+      <br />
+      <Pen />
+      <br />
+      <p>Illustrator</p>
+      <p>Photoshop</p>
+      <p>XD</p>
+      <br />
+      <div className="Skills">
+        <Code />
+        <div className="Grid">
+          <p>JavaScript</p>
+          <p>HTML/CSS</p>
+          <p>React</p>
+          <p>FireBase</p>
+          <p>Git</p>
+          <p>Redux</p>
+          <p>SASS</p>
+          <p>GitHub</p>
+          <p>Router</p>
+        </div>
+      </div>
+      <h1>What do I do?</h1>
+      <br />
+      <div className="Skills2">
+        <h2>Web Design</h2>
+        <p>
+          Minimalist and clean, where every stroke exists for a purpose. All
+          about your website, including the content, the way it looks, and the
+          way it works - I’ve got you covered.
+        </p>
+        <h2>Frontend Development</h2>
+        <p>
+          I will implement the design so the website runs smoothly in different
+          browsers, different operating systems and different devices, from
+          widescreen monitors to mobile phones.
+        </p>
+      </div>
+    </Fragment>
+  );
+}
---
+++ src/Components/UI/Footer/Footer.js
@@ -0,0 +1,69 @@
+import React, { Fragment } from "react";
+import { useSpring } from "react-spring";
+import { ReactComponent as Arrow } from "../../../Assets/Arrow.svg";
+import { ReactComponent as Dribble } from "../../../Assets/Dribbble.svg";
+import { ReactComponent as Facebook } from "../../../Assets/Facebook.svg";
+import { ReactComponent as LinkedIn } from "../../../Assets/LinkedIn.svg";
+import { ReactComponent as Instagram } from "../../../Assets/Instagram.svg";
+import { ReactComponent as GitHub } from "../../../Assets/GitHub.svg";
+export default function Footer() {
+  const [, setY] = useSpring(() => ({ y: 0 }));
+  return (
+    <Fragment>
+      <button
+        className="Scroll"
+        onClick={() => {
+          setY({
+            y: 0,
+            reset: true,
+            from: { y: window.scrollY },
+            onFrame: props => window.scroll(0, props.y)
+          });
+        }}
+      >
+        <Arrow />
+      </button>
+
+      <div className="Footer">
+        <li>
+          <a
+            href="https://dribbble.com/igorizviekov"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Dribble />
+          </a>
+          <a
+            href="https://www.facebook.com/igorizviekov"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Facebook />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/igorizviekov/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedIn />
+          </a>
+          <a
+            href="https://www.instagram.com/igorizviekov/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Instagram />
+          </a>
+          <a
+            href="https://github.com/igorizviekov"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHub />
+          </a>
+        </li>
+        <p>IGOR IZVIEKOV 2020</p>
+      </div>
+    </Fragment>
+  );
+}

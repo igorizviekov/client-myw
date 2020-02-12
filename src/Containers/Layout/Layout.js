db6451373764afdd61b149401aa277f7f3da2183
Igor Izviekov <igorizviekov@gmail.com>
2020-02-12 19:33:15 +0200

    finished mobile version of home page

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../../Components/UI/Nav/Nav";
+import Footer from "../../Components/UI/Footer/Footer";
 import Home from "../../Pages/Home/Home";
 import Portfolio from "../../Pages/Portfolio/Portfolio";
 import Contact from "../../Pages/Contact/Contact";
@@ -13,6 +14,7 @@ const Layout = () => {
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
       </Switch>
+      <Footer />
     </div>
   );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "../../Components/Main/Main";
 import About from "../../Components/About/About";
+import Skills from "../../Components/Skills/Skills";
 import Button from "../../Components/UI/Button/Button";
 import "../../Style/Main.css";
 import { useHistory } from "react-router-dom";
@@ -14,6 +15,7 @@ export default function Home() {
       <Main />
       <Button clicked={portfolioButton}>PORTFOLIO</Button>
       <About />
+      <Skills />
     </div>
   );
 }
